261720f5d8311269d8ea34ace1ea2c833f0273c1
ialtamirano <ivan.altamirano@gmail.com>
2012-07-14 03:16:59 -0700

    Update master

--- public/app/js/services.js
+++ public/app/js/services.js
@@ -14,7 +14,7 @@ MyApp.factory('Cliente', function($resource) {
  	 return $resource(   host+'clientes/:clienteId', {}, {
  	 		     get   : { method:'GET'  },
  	 		     query : { method:'GET'  , isArray:true},
- 	 		     update  : { method:'POST' },
+ 	 		     new  : { method:'POST' },
  	 		     update  : { method:'PUT' },
  	 		     
  	 		     remove: { method:''     }
