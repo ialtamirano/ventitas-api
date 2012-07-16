30c7e96c4b3600b6c21ebe13821a86a024869b06
ialtamirano <ivan.altamirano@gmail.com>
2012-07-16 15:08:44 -0700

    Update master

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -9,6 +9,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
     $routeProvider.when('/ventas', {templateUrl: '/app/partials/ventas.html', controller: VentasCtrl});
     $routeProvider.when('/productos', {templateUrl: '/app/partials/productos.html', controller: ProductosCtrl});
     $routeProvider.when('/clientes', {templateUrl: '/app/partials/clientes.html', controller: ClientesCtrl});
+    $routeProvider.when('/clientes/new', {templateUrl: '/app/partials/clientes-modal.html', controller: ClientesNewCtrl});
     $routeProvider.when('/clientes/edit/:id', {templateUrl: '/app/partials/clientes-modal.html', controller: ClientesEditCtrl});
     $routeProvider.when('/clientes/delete/:id', {templateUrl: '/app/partials/clientes-modal.html', controller: ClientesEditCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
