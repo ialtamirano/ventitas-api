d8cb763de691f8f35017eb9f8b13033bfa8fb139
ialtamirano <ivan.altamirano@gmail.com>
2012-07-16 08:49:19 -0700

    Update master

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -4,12 +4,12 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'app/partials/home.html', controller: HomeCtrl});
-    $routeProvider.when('/dashboard', {templateUrl: 'app/partials/dashboard.html', controller: DashboardCtrl});
-    $routeProvider.when('/ventas', {templateUrl: 'app/partials/ventas.html', controller: VentasCtrl});
-    $routeProvider.when('/productos', {templateUrl: 'app/partials/productos.html', controller: ProductosCtrl});
-    $routeProvider.when('/clientes', {templateUrl: 'app/partials/clientes.html', controller: ClientesCtrl});
-    $routeProvider.when('/clientes/edit/:id', {templateUrl: 'app/partials/clientes-modal.html', controller: ClientesEditCtrl});
-    $routeProvider.when('/clientes/delete/:id', {templateUrl: 'app/partials/clientes-modal.html', controller: ClientesEditCtrl});
+    $routeProvider.when('/home', {templateUrl: '/partials/home.html', controller: HomeCtrl});
+    $routeProvider.when('/dashboard', {templateUrl: '/partials/dashboard.html', controller: DashboardCtrl});
+    $routeProvider.when('/ventas', {templateUrl: '/partials/ventas.html', controller: VentasCtrl});
+    $routeProvider.when('/productos', {templateUrl: '/partials/productos.html', controller: ProductosCtrl});
+    $routeProvider.when('/clientes', {templateUrl: '/partials/clientes.html', controller: ClientesCtrl});
+    $routeProvider.when('/clientes/edit/:id', {templateUrl: '/partials/clientes-modal.html', controller: ClientesEditCtrl});
+    $routeProvider.when('/clientes/delete/:id', {templateUrl: '/partials/clientes-modal.html', controller: ClientesEditCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
